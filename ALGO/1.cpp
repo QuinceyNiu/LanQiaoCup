84fe5197e4cf33d29d6a38bd4e836ebdfef37813
QuinceyNiu <490332924@qq.com>
2020-05-21 16:33:26 +0800

    1

---
+++ ALGO/1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include<stdio.h>
+#include <algorithm>
+#include<string.h>
+#include<cstring>
+#define ll long long
+using namespace std;
+
+int a[100005],b[100005];
+
+int main()
+{
+    int n,m,l,r,k;
+    while(~scanf("%d",&n))
+    {
+        for(int i=1;i<=n;i++)
+        {
+            scanf("%d",&a[i]);
+        }
+        scanf("%d",&m);
+        while(m--)
+        {
+            int cnt=0;
+            memcpy(b,a,sizeof(a));///每次查询都要改变数组的数据顺序，把b当作副本使用，每次结束复制回a的数据
+            scanf("%d%d%d",&l,&r,&k);
+            sort(b+l,b+r+1);
+            for(int i=r;i>=l;i--)
+            {
+                cnt++;              ///描述错误之处，第k大，用数组排序
+                                    ///如果相同的数，也直接跳过，算做另外一大，没有考虑相同的数才能ac
+                if(cnt==k)
+                {
+                    printf("%d\n",b[i]);
+                    break;
+                }
+            }
+        }
+    }
+    return 0;
+}
